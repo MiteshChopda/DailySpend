a1d8045a8e2457e54aec8b3e73b9ab3817386cb5
miteshchopda <miteshchopda00@gmail.com>
2025-12-19 08:59:25 +0000

    added pagination in Table

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {BACKEND_URL} from '../config.js'
+import { BACKEND_URL } from '../config.js'
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -17,152 +17,115 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Fade from '@mui/material/Fade';
+import Pagination from '@mui/material/Pagination';
 
-
-/*
-records:[
-    {
-        amount: 1250,
-        changeInBalance: "spent",
-        created_at: "2025-12-12T08:22:01.536Z",
-        title: "Campus Sliders",
-        __v: 0,
-        _id: "693bd0a9fb6b02509adcae03"
-    },
-    ...]
-*/
+/* records:[ { amount: 1250, changeInBalance: "spent", created_at: "2025-12-12T08:22:01.536Z", title: "Campus Sliders", __v: 0, _id: "693bd0a9fb6b02509adcae03" },...] */
 export default function SimpleTable({ records }) {
     const [rows, setRows] = useState(records)
     const Columns = [
-        {
-            name: "Record Title",
-            id: "title", // to uniquly identify coln in future
-        },
-        {
-            name: "Amount",
-            id: "amount", // to uniquly identify coln in future
-        },
-        {
-            name: "Change",
-            id: "changeInBalance", // to uniquly identify coln in future
-        },
-        {
-            name: "Date",
-            id: "created_at", // to uniquly identify coln in future
-        },
-        {
-            name: "Action",
-            id: "action", // to uniquly identify coln in future
-        }
-
+        { name: "Record Title", id: "title", },
+        { name: "Amount", id: "amount", },
+        { name: "Change", id: "changeInBalance", },
+        { name: "Date", id: "created_at", },
+        { name: "Action", id: "action", }
     ]
+    const rowsPerPage = 10;
+    const totalPages = Math.ceil(rows.length / rowsPerPage);
+    const [page, setPage] = useState(0);
+    const slicedRows = rows.slice(page * rowsPerPage, (page + 1) * rowsPerPage);
+    const handlePageChange = (event, value) => {
+        setPage(value - 1);
+    };
 
     // Delete Record
     const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
     const [selectedRow, setSelectedRow] = useState(null);
-
     const handleDeleteClickOpen = (row) => {
         setSelectedRow(row);
         setOpenDeleteDialog(true);
     };
-
     const handleDeleteClose = () => {
         setOpenDeleteDialog(false);
     };
-
     const handleDelete = (_id, title) => {
         const id = _id
         fetch(`${BACKEND_URL}/api/records/delete/` + id, {
             method: 'DELETE',
         })
-            .then(res => { res.json(); })
+            .then(res => {
+                res.json();
+            })
             .then(data => {
                 console.log("delete res:", data)
-                setRows((prevRows) =>
-                    prevRows.filter((row) => row._id !== id)
-                );
+                setRows((prevRows) => prevRows.filter((row) => row._id !== id));
                 window.alert(title + " deleted!")
             })
             .catch(err => window.alert(err))
-
     }
+
+
     return (
         <>
             <TableContainer component={Paper}>
-                <Table
-                    sx={{ gap: 0 }}
-                    aria-label="simple table">
-
+                <Table sx={{ gap: 0 }} size="small" aria-label="a dense table">
                     <TableHead>
                         <TableRow>
                             {Columns.map(col => (
                                 <TableCell align={col.id != "title" ? "right" : ""} key={col.id}>
-                                    <Typography
-
-                                        sx={{ color: "black", fontWeight: "bold" }} >
+                                    <Typography sx={{ color: "black", fontWeight: "bold" }} >
                                         {col.name}
                                     </Typography>
                                 </TableCell>
                             ))}
                         </TableRow>
-
                     </TableHead>
                     <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row._id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
+                        {slicedRows.map((row) => (
+                            <TableRow key={row._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
                                 {/* title */}
                                 <TableCell component="th" scope="row">
                                     {row.title}
                                 </TableCell>
-
                                 {/* amount */}
                                 <TableCell align="right">
                                     {row.amount}
                                 </TableCell>
-
                                 {/* change */}
                                 <TableCell align="right">
                                     <Typography color={row.changeInBalance != "spent" ? "green" : "red"} >
                                         {row.changeInBalance}
                                     </Typography>
                                 </TableCell>
-
                                 {/* date */}
-                                <TableCell align="right">{new Date(row.created_at).toLocaleDateString(
-                                    "en-GB",
-                                    { day: "numeric", month: "short" }
-                                )}
-                                </TableCell>
-
+                                <TableCell align="right">{new Date(row.created_at).toLocaleDateString("en-GB", { day: "numeric", month: "short" })}</TableCell>
                                 {/* action */}
                                 <TableCell align="right">
-
-                                    <IconButton color="primary" onClick={() => {
-                                        handleDeleteClickOpen(row)
-                                    }}>
+                                    <IconButton color="primary" onClick={() => { handleDeleteClickOpen(row) }}>
                                         <DeleteIcon />
                                     </IconButton>
-
                                 </TableCell>
                             </TableRow>
                         ))}
-
                     </TableBody>
                 </Table>
-            </TableContainer >
-
+            </TableContainer>
+            <Pagination
+                count={totalPages}
+                page={page + 1}
+                onChange={handlePageChange}
+                variant="outlined"
+                shape="rounded"
+            />
             <Dialog
                 open={openDeleteDialog}
                 TransitionComponent={Fade}
                 slotProps={{
                     backdrop: {
                         sx: {
-                            backgroundColor: '#352f2f71', // Adjust opacity (0-1)
+                            backgroundColor: '#352f2f71',
                         }
-                    }, paper: {
+                    },
+                    paper: {
                         sx: {
                             borderRadius: "15px",
                         },
@@ -182,12 +145,7 @@ export default function SimpleTable({ records }) {
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleDeleteClose}>Cancel</Button>
-                    <Button
-                        onClick={() => {
-                            handleDeleteClose();
-                            handleDelete(selectedRow._id, selectedRow.title);
-                        }}
-                        autoFocus>
+                    <Button onClick={() => { handleDeleteClose(); handleDelete(selectedRow._id, selectedRow.title); }} autoFocus>
                         Ok
                     </Button>
                 </DialogActions>
